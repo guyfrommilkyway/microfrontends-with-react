0a4df85ae6270134bbdd47c550621792f358f6a0
Almer Tampus <almerflorestampus@gmail.com>
2022-11-27 05:38:09 +0800

    Testing a re-run

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Forcing a re-run!</h1>
+      <h1>Forcing a another re-run!</h1>
       <hr />
       <MarketingApp />
     </div>
